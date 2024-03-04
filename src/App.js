e2c9a8990ef14279c682c69d271aad0a8d2c0fc0
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-03-04 20:45:41 +0100

    Προσθήκη Κώδικα Αρχικής Οθόνης Εφαρμογής

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>🇬🇷🇬🇷🇬🇷</h1>
+      <h1>Ούλε τε και μάλα χαίρε!</h1>
+      <h1>🤩</h1>
+    </main>
   );
 }
 
